e7848174b7fc2d9973311340790053d0e3a0246d
Andre Leibovici <andre@Andres-MacBook-Air.local>
2020-02-10 08:44:35 +1300

    Error handling for Earthquake API error

--- earthquake.py
+++ earthquake.py
@@ -13,12 +13,15 @@ def get_quake(city: str, magnitude_min, period: str):
         earthquakes.connect()   # Establish connectivity to USGS to avoid cached results
         report = earthquakes.get_report(period, 'all')  # Ger earthquake report
     except:
-        logger.warning('Unexpected error: ' + str(sys.exc_info()[0]))
-        raise
-
-    for i in report['earthquakes']:  # Iterate earthquake report
-        if re.search(city, i['location_description']):  # Search for matching location (city)
-            logger.info(city + ' earthquake ' + str(i['magnitude']))
-            if i['magnitude'] >= float(magnitude_min):  # Look for earthquake magnitude >= magnitude_min
-                return True
-    return False
+        logger.error('Unexpected error: ' + str(sys.exc_info()[0]))
+        #
+        # Need error handling to notify administrator
+        #
+        return False
+    else:
+        for i in report['earthquakes']:  # Iterate earthquake report
+            if re.search(city, i['location_description']):  # Search for matching location (city)
+                logger.info(city + ' earthquake ' + str(i['magnitude']))
+                if i['magnitude'] >= float(magnitude_min):  # Look for earthquake magnitude >= magnitude_min
+                    return True
+        return False
